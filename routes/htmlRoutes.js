897ecfc8bf9a0cdee52accd427225281120504e3
BGorson <BGorson32@gmail.com>
2019-03-20 21:14:16 -0500

    added post route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,11 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/post", function(req, res) {
+    res.render("post", {
+      msg: "Post here!"
+    });
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
