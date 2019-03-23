051fa064e368d99aee2d6b736a0e0c783c5a15d8
fadln11 <fadeline314@gmail.com>
2019-03-23 11:18:42 -0500

    fix rename db.Example to db.Post

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,14 +3,13 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Post.findAll({}).then(function(dbPosts) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        examples: dbPosts
       });
     });
   });
-
   app.get("/post", function(req, res) {
     res.render("post", {
       msg: "Post here!",
@@ -19,9 +18,9 @@ module.exports = function(app) {
   });
   // Load all posts
   app.get("/posts", function(req, res) {
-    db.Example.findAll({}).then(function(dbExample) {
+    db.Post.findAll({}).then(function(dbPosts) {
       res.render("display-posts", {
-        posts: dbExample
+        posts: dbPosts
       });
     });
   });
@@ -42,9 +41,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Post.findOne({ where: { id: req.params.id } }).then(function(dbPosts) {
       res.render("example", {
-        example: dbExample
+        example: dbPosts
       });
     });
   });
