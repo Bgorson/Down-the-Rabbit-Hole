d09675359f7f9809a176160ccf9f91543ce0f60d
fadln11 <fadeline314@gmail.com>
2019-03-25 20:28:15 -0500

    update and rename seeds.sql

---
+++ models/seeds.sql
@@ -0,0 +1,16 @@
+-- Add User
+INSERT INTO projecttwo.Users (email, password, createdAt, updatedAt) VALUES
+('user_1@email.com', 'password', NOW(), NOW()),
+('user_2@email.com', 'password', NOW(), NOW());
+
+-- Add Posts
+INSERT INTO projecttwo.Posts
+(text, description, category, createdAt, updatedAt, UserId) VALUES
+('title_1', 'description', 'category_1', NOW(), NOW(), 1),
+('title_2', 'description', 'category_1', NOW(), NOW(), 1),
+('title_3', 'description', 'category_2', NOW(), NOW(), 2);
+
+-- Add Comments
+INSERT INTO projecttwo.Comments (text, createdAt, updatedAt, PostId) VALUES
+('comment_1', NOW(), NOW(), 1),
+('comment_2', NOW(), NOW(), 2);
