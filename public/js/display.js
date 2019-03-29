aa96b97694456bf44e61bbae0156e640ca9cb57c
fadln11 <fadeline314@gmail.com>
2019-03-28 22:01:48 -0500

    update frontend category sidenav and main navbar media responsive

--- public/js/display.js
+++ public/js/display.js
@@ -1,12 +1,14 @@
+// Vars
 let userName;
 let login= false;
+
 $(document).ready(function() {
-$.get("/api/user_data").then(function(data) {
+
+  $.get("/api/user_data").then(function(data) {
     userName = data.name
     console.log("user name:" + userName)
     login = data.login
     $(".member-name").text(userName);
   });
 
-})
-
+});
